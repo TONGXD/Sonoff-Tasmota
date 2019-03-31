99afb3cd0dd3ac385d34c816b6474a4d6ea587b9
gemu2015 <gmutz2010@googlemail.com>
2019-03-31 15:36:07 +0200

    ADS1115 optical counter sensor input

--- sonoff/xsns_95_sml.ino
+++ sonoff/xsns_95_sml.ino
@@ -5,7 +5,7 @@ da durch die begrenzte Hardwareunterstützung das software serial nicht optimal 
 ist es mit der originalen TasmotaSerial nicht möglich 3 Zähler gleichzeitig abzufragen
 
 durch Modifikation des Tasmota Serial Drivers sollten jetzt auch mehr als 2 Zähler
-funktionieren. Dazu muss auch die TasmotaSerial-2.3.0 ebenfalls kopiert werden
+funktionieren. Dazu muss auch die modifizierte TasmotaSerial-2.3.0 ebenfalls kopiert werden
 
 jetzt auch mit Unterstützung für Gas und Wasserzähler
 Zähler setzen mit Sensor95 c1 xxx, Sensor95 c2 xxx etc
@@ -45,8 +45,12 @@ die älteren werden nicht mehr unterstützt.
 
 // send this every N seconds
 //#define SML_SEND_SEQ
-// debug counter input to led
-//#define GAS_LED 2
+// debug counter input to led for counter1 and 2
+//#define DEBUG_CNT_LED1 2
+//#define DEBUG_CNT_LED1 2
+
+// use optical counter sensor with AD Converter ADS1115
+//#define ANALOG_OPTO_SENSOR
 
 
 // support für mehr als 2 Meter mit spezieller Tasmota Serial Version
@@ -536,6 +540,232 @@ double median(struct MEDIAN_FILTER* mf, double in) {
   return tbuff[MEDIAN_SIZE/2];
 }
 #endif
+
+#ifdef ANALOG_OPTO_SENSOR
+uint8_t ads1115_up;
+
+// ads1115 driver
+#define SAMPLE_BIT (0x8000)
+
+#define ADS1115_COMP_QUEUE_SHIFT 0
+#define ADS1115_COMP_LATCH_SHIFT 2
+#define ADS1115_COMP_POLARITY_SHIFT 3
+#define ADS1115_COMP_MODE_SHIFT 4
+#define ADS1115_DATA_RATE_SHIFT 5
+#define ADS1115_MODE_SHIFT 8
+#define ADS1115_PGA_SHIFT 9
+#define ADS1115_MUX_SHIFT 12
+
+enum ads1115_comp_queue {
+	ADS1115_COMP_QUEUE_AFTER_ONE = 0,
+	ADS1115_COMP_QUEUE_AFTER_TWO = 0x1 << ADS1115_COMP_QUEUE_SHIFT,
+	ADS1115_COMP_QUEUE_AFTER_FOUR = 0x2 << ADS1115_COMP_QUEUE_SHIFT,
+	ADS1115_COMP_QUEUE_DISABLE = 0x3 << ADS1115_COMP_QUEUE_SHIFT,
+	ADS1115_COMP_QUEUE_MASK = 0x3 << ADS1115_COMP_QUEUE_SHIFT,
+};
+
+enum ads1115_comp_latch {
+	ADS1115_COMP_LATCH_NO = 0,
+	ADS1115_COMP_LATCH_YES = 1 << ADS1115_COMP_LATCH_SHIFT,
+	ADS1115_COMP_LATCH_MASK = 1 << ADS1115_COMP_LATCH_SHIFT,
+};
+
+enum ads1115_comp_polarity {
+	ADS1115_COMP_POLARITY_ACTIVE_LOW = 0,
+	ADS1115_COMP_POLARITY_ACTIVE_HIGH = 1 << ADS1115_COMP_POLARITY_SHIFT,
+	ADS1115_COMP_POLARITY_MASK = 1 << ADS1115_COMP_POLARITY_SHIFT,
+};
+
+enum ads1115_comp_mode {
+	ADS1115_COMP_MODE_WINDOW = 0,
+	ADS1115_COMP_MODE_HYSTERESIS = 1 << ADS1115_COMP_MODE_SHIFT,
+	ADS1115_COMP_MODE_MASK = 1 << ADS1115_COMP_MODE_SHIFT,
+};
+
+enum ads1115_data_rate {
+	ADS1115_DATA_RATE_8_SPS = 0,
+	ADS1115_DATA_RATE_16_SPS = 0x1 << ADS1115_DATA_RATE_SHIFT,
+	ADS1115_DATA_RATE_32_SPS = 0x2 << ADS1115_DATA_RATE_SHIFT,
+	ADS1115_DATA_RATE_64_SPS = 0x3 << ADS1115_DATA_RATE_SHIFT,
+	ADS1115_DATA_RATE_128_SPS = 0x4 << ADS1115_DATA_RATE_SHIFT,
+	ADS1115_DATA_RATE_250_SPS = 0x5 << ADS1115_DATA_RATE_SHIFT,
+	ADS1115_DATA_RATE_475_SPS = 0x6 << ADS1115_DATA_RATE_SHIFT,
+	ADS1115_DATA_RATE_860_SPS = 0x7 << ADS1115_DATA_RATE_SHIFT,
+	ADS1115_DATA_RATE_MASK = 0x7 << ADS1115_DATA_RATE_SHIFT,
+};
+
+enum ads1115_mode {
+	ADS1115_MODE_CONTINUOUS = 0,
+	ADS1115_MODE_SINGLE_SHOT = 1 << ADS1115_MODE_SHIFT,
+	ADS1115_MODE_MASK = 1 << ADS1115_MODE_SHIFT,
+};
+
+enum ads1115_pga {
+	ADS1115_PGA_TWO_THIRDS = 0,
+	ADS1115_PGA_ONE = 0x1 << ADS1115_PGA_SHIFT,
+	ADS1115_PGA_TWO = 0x2 << ADS1115_PGA_SHIFT,
+	ADS1115_PGA_FOUR = 0x3 << ADS1115_PGA_SHIFT,
+	ADS1115_PGA_EIGHT = 0x4 << ADS1115_PGA_SHIFT,
+	ADS1115_PGA_SIXTEEN = 0x5 << ADS1115_PGA_SHIFT,
+	ADS1115_PGA_MASK = 0x7 << ADS1115_PGA_SHIFT,
+};
+
+enum ads1115_mux {
+	ADS1115_MUX_DIFF_AIN0_AIN1 = 0,
+	ADS1115_MUX_DIFF_AIN0_AIN3 = 0x1 << ADS1115_MUX_SHIFT,
+	ADS1115_MUX_DIFF_AIN1_AIN3 = 0x2 << ADS1115_MUX_SHIFT,
+	ADS1115_MUX_DIFF_AIN2_AIN3 = 0x3 << ADS1115_MUX_SHIFT,
+	ADS1115_MUX_GND_AIN0 = 0x4 << ADS1115_MUX_SHIFT,
+	ADS1115_MUX_GND_AIN1 = 0x5 << ADS1115_MUX_SHIFT,
+	ADS1115_MUX_GND_AIN2 = 0x6 << ADS1115_MUX_SHIFT,
+	ADS1115_MUX_GND_AIN3 = 0x7 << ADS1115_MUX_SHIFT,
+	ADS1115_MUX_MASK = 0x7 << ADS1115_MUX_SHIFT,
+};
+
+class ADS1115 {
+public:
+	ADS1115(uint8_t address = 0x48);
+
+	void begin();
+	uint8_t trigger_sample();
+	uint8_t reset();
+	bool is_sample_in_progress();
+	int16_t read_sample();
+	float sample_to_float(int16_t val);
+	float read_sample_float();
+
+	void set_comp_queue(enum ads1115_comp_queue val) { set_config(val, ADS1115_COMP_QUEUE_MASK); }
+	void set_comp_latching(enum ads1115_comp_latch val) { set_config(val, ADS1115_COMP_LATCH_MASK); }
+	void set_comp_polarity(enum ads1115_comp_polarity val) { set_config(val, ADS1115_COMP_POLARITY_MASK); }
+	void set_comp_mode(enum ads1115_comp_mode val) { set_config(val, ADS1115_COMP_MODE_MASK); }
+	void set_data_rate(enum ads1115_data_rate val) { set_config(val, ADS1115_DATA_RATE_MASK); }
+	void set_mode(enum ads1115_mode val) { set_config(val, ADS1115_MODE_MASK); }
+	void set_pga(enum ads1115_pga val) { set_config(val, ADS1115_PGA_MASK); m_voltage_range = val >> ADS1115_PGA_SHIFT; }
+	void set_mux(enum ads1115_mux val) { set_config(val, ADS1115_MUX_MASK); }
+
+private:
+	void set_config(uint16_t val, uint16_t mask) {
+		m_config = (m_config & ~mask) | val;
+	}
+
+	uint8_t write_register(uint8_t reg, uint16_t val);
+	uint16_t read_register(uint8_t reg);
+
+	uint8_t m_address;
+	uint16_t m_config;
+	int m_voltage_range;
+};
+
+
+enum ads1115_register {
+	ADS1115_REGISTER_CONVERSION = 0,
+	ADS1115_REGISTER_CONFIG = 1,
+	ADS1115_REGISTER_LOW_THRESH = 2,
+	ADS1115_REGISTER_HIGH_THRESH = 3,
+};
+
+#define FACTOR 32768.0
+static float ranges[] = { 6.144 / FACTOR, 4.096 / FACTOR, 2.048 / FACTOR, 1.024 / FACTOR, 0.512 / FACTOR, 0.256 / FACTOR};
+
+ADS1115::ADS1115(uint8_t address)
+{
+        m_address = address;
+        m_config = ADS1115_COMP_QUEUE_AFTER_ONE |
+                   ADS1115_COMP_LATCH_NO |
+                   ADS1115_COMP_POLARITY_ACTIVE_LOW |
+                   ADS1115_COMP_MODE_WINDOW |
+                   ADS1115_DATA_RATE_128_SPS |
+                   ADS1115_MODE_SINGLE_SHOT |
+                   ADS1115_MUX_GND_AIN0;
+        set_pga(ADS1115_PGA_ONE);
+}
+
+uint8_t ADS1115::write_register(uint8_t reg, uint16_t val)
+{
+        Wire.beginTransmission(m_address);
+        Wire.write(reg);
+        Wire.write(val>>8);
+        Wire.write(val & 0xFF);
+        return Wire.endTransmission();
+}
+
+uint16_t ADS1115::read_register(uint8_t reg)
+{
+        Wire.beginTransmission(m_address);
+        Wire.write(reg);
+        Wire.endTransmission();
+
+        uint8_t result = Wire.requestFrom((int)m_address, 2, 1);
+        if (result != 2) {
+                Wire.flush();
+                return 0;
+        }
+
+        uint16_t val;
+
+        val = Wire.read() << 8;
+        val |= Wire.read();
+        return val;
+}
+
+void ADS1115::begin()
+{
+        Wire.begin();
+}
+
+uint8_t ADS1115::trigger_sample()
+{
+        return write_register(ADS1115_REGISTER_CONFIG, m_config | SAMPLE_BIT);
+}
+
+uint8_t ADS1115::reset()
+{
+	Wire.beginTransmission(0);
+	Wire.write(0x6);
+	return Wire.endTransmission();
+}
+
+bool ADS1115::is_sample_in_progress()
+{
+	uint16_t val = read_register(ADS1115_REGISTER_CONFIG);
+	return (val & SAMPLE_BIT) == 0;
+}
+
+int16_t ADS1115::read_sample()
+{
+        return read_register(ADS1115_REGISTER_CONVERSION);
+}
+
+float ADS1115::sample_to_float(int16_t val)
+{
+	return val * ranges[m_voltage_range];
+}
+
+float ADS1115::read_sample_float()
+{
+	return sample_to_float(read_sample());
+}
+
+ADS1115 adc;
+
+void ADS1115_init(void) {
+
+  ads1115_up=0;
+  if (!i2c_flg) return;
+
+  adc.begin();
+  adc.set_data_rate(ADS1115_DATA_RATE_128_SPS);
+  adc.set_mode(ADS1115_MODE_CONTINUOUS);
+  adc.set_mux(ADS1115_MUX_GND_AIN1);
+  adc.set_pga(ADS1115_PGA_TWO);
+
+  int16_t val = adc.read_sample();
+  ads1115_up=1;
+}
+
+#endif
+
+
 
 
 // dump to log zeigt serielle Daten zu Testzwecken in der Konsole an
@@ -1201,28 +1431,46 @@ void SML_Show(boolean json) {
     if (json) snprintf_P(mqtt_data, sizeof(mqtt_data), "%s}", mqtt_data);
 }
 
+struct SML_COUNTER {
+  uint8_t sml_cnt_debounce;
+  uint8_t sml_cnt_old_state;
+  uint32_t sml_cnt_last_ts;
+#ifdef ANALOG_OPTO_SENSOR
+  int16_t ana_curr;
+  int16_t ana_max;
+  int16_t ana_min;
+  int16_t ana_cmpl;
+  int16_t ana_cmph;
+#endif
+} sml_counters[MAX_COUNTERS];
 
-uint8_t sml_cnt_debounce[MAX_COUNTERS];
-uint8_t sml_cnt_old_state[MAX_COUNTERS];
-uint32_t sml_cnt_last_ts[MAX_COUNTERS];
 
 void SML_Init(void) {
   uint8_t cindex=0;
   // preloud counters
   for (byte i = 0; i < MAX_COUNTERS; i++) {
       RtcSettings.pulse_counter[i]=Settings.pulse_counter[i];
-      sml_cnt_last_ts[i]=millis();
+      sml_counters[i].sml_cnt_last_ts=millis();
   }
   for (uint8_t meters=0; meters<METERS_USED; meters++) {
     if (meter_desc[meters].type=='c') {
-        // counters, set to input with pullup
-        if (meter_desc[meters].flag&1) {
-          pinMode(meter_desc[meters].srcpin,INPUT_PULLUP);
+        if (meter_desc[meters].flag&2) {
+          // analog mode
+#ifdef ANALOG_OPTO_SENSOR
+          ADS1115_init();
+          sml_counters[cindex].ana_max=-32768;
+          sml_counters[cindex].ana_min=+32767;
+#endif
         } else {
-          pinMode(meter_desc[meters].srcpin,INPUT);
+          // counters, set to input with pullup
+          if (meter_desc[meters].flag&1) {
+            pinMode(meter_desc[meters].srcpin,INPUT_PULLUP);
+          } else {
+            pinMode(meter_desc[meters].srcpin,INPUT);
+          }
+          InjektCounterValue(meters,RtcSettings.pulse_counter[cindex]);
+          cindex++;
         }
-        InjektCounterValue(meters,RtcSettings.pulse_counter[cindex]);
-        cindex++;
     } else {
       // serial input, init
       if (!meter_desc[meters].srcpin || meter_desc[meters].srcpin==3) {
@@ -1252,34 +1500,61 @@ uint16_t meters,cindex=0;
     uint32_t ctime=millis();
     if (meter_desc[meters].type=='c') {
       // poll for counters and debouce
-      if (ctime-sml_cnt_last_ts[cindex]>meter_desc[meters].params) {
-        // poll
-        sml_cnt_last_ts[cindex]=millis();
-  #ifdef GAS_LED
-        pinMode(GAS_LED, OUTPUT);
-        if (digitalRead(meter_desc[meters].srcpin)) {
-            digitalWrite(GAS_LED,HIGH);
+      if (ctime-sml_counters[cindex].sml_cnt_last_ts>meter_desc[meters].params) {
+        sml_counters[cindex].sml_cnt_last_ts=millis();
+
+        if (meter_desc[meters].flag&2) {
+          // analog mode, get next value
+#ifdef ANALOG_OPTO_SENSOR
+          if (ads1115_up) {
+            int16_t val = adc.read_sample();
+            if (val>sml_counters[cindex].ana_max) sml_counters[cindex].ana_max=val;
+            if (val<sml_counters[cindex].ana_min) sml_counters[cindex].ana_min=val;
+            sml_counters[cindex].ana_curr=val;
+            int16_t range=sml_counters[cindex].ana_max-sml_counters[cindex].ana_min;
+
+          }
+#endif
         } else {
-            digitalWrite(GAS_LED,LOW);
-        }
+          // poll digital input
+  #ifdef DEBUG_CNT_LED1
+          if (cindex==0) {
+            pinMode(DEBUG_CNT_LED1, OUTPUT);
+            if (digitalRead(meter_desc[meters].srcpin)) {
+              digitalWrite(DEBUG_CNT_LED1,HIGH);
+            } else {
+              digitalWrite(DEBUG_CNT_LED1,LOW);
+            }
+          }
   #endif
-        uint8_t state;
-        sml_cnt_debounce[cindex]<<=1;
-        sml_cnt_debounce[cindex]|=(digitalRead(meter_desc[meters].srcpin)&1)|0x80;
-        if (sml_cnt_debounce[cindex]==0xc0) {
-          // is 1
-          state=1;
-        } else {
-          // is 0, means switch down
-          state=0;
-        }
-        if (sml_cnt_old_state[cindex]!=state) {
-          // state has changed
-          sml_cnt_old_state[cindex]=state;
-          if (state==0) {
-            // inc counter
-            RtcSettings.pulse_counter[cindex]++;
-            InjektCounterValue(meters,RtcSettings.pulse_counter[cindex]);
+  #ifdef DEBUG_CNT_LED2
+          if (cindex==1) {
+            pinMode(DEBUG_CNT_LED2, OUTPUT);
+            if (digitalRead(meter_desc[meters].srcpin)) {
+              digitalWrite(DEBUG_CNT_LED2,HIGH);
+            } else {
+              digitalWrite(DEBUG_CNT_LED2,LOW);
+            }
+          }
+  #endif
+          uint8_t state;
+          sml_counters[cindex].sml_cnt_debounce<<=1;
+          sml_counters[cindex].sml_cnt_debounce|=(digitalRead(meter_desc[meters].srcpin)&1)|0x80;
+          if (sml_counters[cindex].sml_cnt_debounce==0xc0) {
+            // is 1
+            state=1;
+          } else {
+            // is 0, means switch down
+            state=0;
+          }
+          if (sml_counters[cindex].sml_cnt_old_state!=state) {
+            // state has changed
+            sml_counters[cindex].sml_cnt_old_state=state;
+            if (state==0) {
+              // inc counter
+              RtcSettings.pulse_counter[cindex]++;
+              InjektCounterValue(meters,RtcSettings.pulse_counter[cindex]);
+            }
           }
         }
       }
@@ -1367,6 +1642,7 @@ void SML_CounterSaveState(void) {
       Settings.pulse_counter[i] = RtcSettings.pulse_counter[i];
   }
 }
+
 /*********************************************************************************************\
  * Interface
 \*********************************************************************************************/
